59d4e7fa1f6516cbd12abb8edef9cb0ae67fa987
hwang <nmc2711@naver.com>
2022-10-04 13:34:26 +0900

    resize

--- components/page.tsx
+++ components/page.tsx
@@ -22,7 +22,7 @@ const Page = ({ title, children }: Props) => (
 			 * Padding top = `appbar` height
 			 * Padding bottom = `bottom-nav` height
 			 */
-			className='mx-auto max-w-screen-md pt-20 pb-16 px-safe sm:pb-0'
+			className='relative mx-auto max-w-screen-md pt-20 pb-16 px-safe sm:pb-0 overflow-x-hidden h-[100vh]'
 		>
 			<div className='p-6'>{children}</div>
 		</main>
