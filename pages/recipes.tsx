5b58e350a2444b71e2b2181de7e14d7259b2c037
hwang <nmc2711@naver.com>
2022-10-08 13:55:28 +0900

    grahql test

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -1,31 +1,67 @@
 import Page from '@/components/page'
 import Section from '@/components/section'
+import Image from 'next/image'
 
-const Recipes = () => (
-	<Page>
-		<Section>
-			<h2 className='text-xl font-semibold'>Components</h2>
+import { GraphQLClient, gql } from 'graphql-request'
 
-			<div className='mt-2'>
-				<p className='text-zinc-600 dark:text-zinc-400'>
-				</p>
-			</div>
-		</Section>
-
-		<Section>
-			<h3 className='font-medium'>o</h3>
-
-			<ul className='list-disc space-y-2 px-6 py-2'>
-				<li className='text-sm text-zinc-600 dark:text-zinc-400'>
-
-				</li>
-
-				<li className='text-sm text-zinc-600 dark:text-zinc-400'>
-
-				</li>
-			</ul>
-		</Section>
-	</Page>
+const graphcms = new GraphQLClient(
+	'https://api-ap-northeast-1.hygraph.com/v2/cl8zbbyjl0vib01tcglp1asau/master'
 )
 
-export default Recipes
+const Query = gql`
+	{
+		posts {
+			publishedAt
+			title
+			summary
+			updatedAt
+			content {
+				html
+			}
+			coverPhoto {
+				url
+			}
+		}
+	}
+`
+
+export async function getStaticProps() {
+	const { posts } = await graphcms.request(Query)
+	return {
+		props: {
+			posts,
+		},
+		revalidate: 10,
+	}
+}
+
+export default function Recipes({ posts }: any) {
+	console.log(posts)
+	return (
+		<Page>
+			<Section>
+				<h2 className='text-xl font-semibold'>Components</h2>
+
+				<div className='mt-2'>
+					<p className='text-zinc-600 dark:text-zinc-400'></p>
+				</div>
+			</Section>
+
+			<Section>
+				<h3 className='font-medium'>그랲ql 글 테스트</h3>
+
+				<ul className='list-disc space-y-2 px-6 py-2'>
+					{posts.map((item: any, key: any) => {
+						console.log(item)
+						return (
+							<li key={key}>
+								{item.summary}
+								<img src={item.coverPhoto.url} />
+							</li>
+						)
+					})}
+				</ul>
+			</Section>
+		</Page>
+	)
+}
